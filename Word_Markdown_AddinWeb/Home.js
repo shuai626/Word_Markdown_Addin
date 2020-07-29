158f4ac57eac369ff9866c7d5e1e9465bc589f12
Sean Lin <t-shulin@microsoft.com>
2020-07-29 17:23:14 -0600

    Codeblock button generates a gray table that takes up the line

--- Word_Markdown_AddinWeb/Home.js
+++ Word_Markdown_AddinWeb/Home.js
@@ -9,9 +9,7 @@
             // Use this to check whether the API is supported in the Word client.
             if (Office.context.requirements.isSetSupported('WordApi', '1.1')) {
                 // Do something that is only available via the new APIs
-                $('#emerson').click(insertEmersonQuoteAtSelection);
-                $('#checkhov').click(insertChekhovQuoteAtTheBeginning);
-                $('#proverb').click(insertChineseProverbAtTheEnd);
+                $('#add').click(createCodeBlock);
                 $('#supportedVersion').html('This code is using Word 2016 or later.');
             }
             else {
@@ -21,8 +19,9 @@
         });
     });
 
-    function insertEmersonQuoteAtSelection() {
+    function createCodeBlock() {
         Word.run(function (context) {
+            let bgColor = "#e0e0e0";
 
             // Create a proxy object for the document.
             var thisDocument = context.document;
@@ -32,58 +31,14 @@
             var range = thisDocument.getSelection();
 
             // Queue a command to replace the selected text.
-            range.insertText('"Hitch your wagon to a star."\n', Word.InsertLocation.replace);
+            var table = range.insertTable(1, 1, Word.InsertLocation.before);
+            table.getBorder(Word.BorderLocation.all).color = bgColor;
+            table.shadingColor = bgColor;
 
             // Synchronize the document state by executing the queued commands,
             // and return a promise to indicate task completion.
             return context.sync().then(function () {
-                console.log('Added a quote from Ralph Waldo Emerson.');
-            });
-        })
-            .catch(function (error) {
-                console.log('Error: ' + JSON.stringify(error));
-                if (error instanceof OfficeExtension.Error) {
-                    console.log('Debug info: ' + JSON.stringify(error.debugInfo));
-                }
-            });
-    }
-
-    function insertChekhovQuoteAtTheBeginning() {
-        Word.run(function (context) {
-
-            // Create a proxy object for the document body.
-            var body = context.document.body;
-
-            // Queue a command to insert text at the start of the document body.
-            body.insertText('"Knowledge is of no value unless you put it into practice."\n', Word.InsertLocation.start);
-
-            // Synchronize the document state by executing the queued commands,
-            // and return a promise to indicate task completion.
-            return context.sync().then(function () {
-                console.log('Added a quote from Anton Chekhov.');
-            });
-        })
-            .catch(function (error) {
-                console.log('Error: ' + JSON.stringify(error));
-                if (error instanceof OfficeExtension.Error) {
-                    console.log('Debug info: ' + JSON.stringify(error.debugInfo));
-                }
-            });
-    }
-
-    function insertChineseProverbAtTheEnd() {
-        Word.run(function (context) {
-
-            // Create a proxy object for the document body.
-            var body = context.document.body;
-
-            // Queue a command to insert text at the end of the document body.
-            body.insertText('"To know the road ahead, ask those coming back."\n', Word.InsertLocation.end);
-
-            // Synchronize the document state by executing the queued commands,
-            // and return a promise to indicate task completion.
-            return context.sync().then(function () {
-                console.log('Added a quote from a Chinese proverb.');
+                console.log('Added a 1x1 table.');
             });
         })
             .catch(function (error) {
